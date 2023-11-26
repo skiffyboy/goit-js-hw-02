3d7546761dcac2d2a08fb6b89697f8c7a8a614ec
ITM-1506 <113722564+ScarletCyph3r@users.noreply.github.com>
2023-11-26 16:54:53 +0200

    task2 done

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function formatMessage(message, maxLength) {
+  let messageLength = message.length;
+  let messageCut = message.slice(0, maxLength);
+  if (messageLength <= maxLength) {
+    return message;
+  } else if (messageLength > maxLength) {
+    return messageCut + "...";
+  }
+}
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
